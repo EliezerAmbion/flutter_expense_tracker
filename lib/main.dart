44888af6d2945423e747559893a6aa1bf81a1948
EliezerAmbion <eliezerambion@gmail.com>
2022-11-08 21:17:14 +0800

    Adding transactions

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
-import 'transaction.dart';
+import 'widgets/user_transactions.dart';
 
 void main() {
   runApp(MyApp());
@@ -19,20 +18,8 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> transactions = [
-    Transaction(
-      id: 't1',
-      title: 'New Shoes',
-      amount: 69.99,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Weekly Grocery',
-      amount: 10.99,
-      date: DateTime.now(),
-    ),
-  ];
+  late String titleInput;
+  late String amountInput;
 
   @override
   Widget build(BuildContext context) {
@@ -52,79 +39,7 @@ class MyHomePage extends StatelessWidget {
               child: Text('CHART'),
             ),
           ),
-          Card(
-            elevation: 5,
-            child: Container(
-              padding: const EdgeInsets.all(10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  const TextField(
-                    decoration: InputDecoration(labelText: 'Title: '),
-                  ),
-                  const TextField(
-                    decoration: InputDecoration(labelText: 'Amount: '),
-                  ),
-                  TextButton(
-                    onPressed: () {},
-                    child: const Text('Add Transaction'),
-                    style: TextButton.styleFrom(foregroundColor: Colors.amber),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Column(
-            children: transactions.map((tx) {
-              return Card(
-                elevation: 5,
-                // with using a class model, you can have an intelisense that knows the property of that class, ex: .title
-                child: Row(
-                  children: [
-                    Container(
-                      margin: const EdgeInsets.symmetric(
-                        vertical: 10,
-                        horizontal: 15,
-                      ),
-                      padding: const EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.purple,
-                          width: 2,
-                        ),
-                      ),
-                      child: Text(
-                        'P${tx.amount}',
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Colors.purple,
-                        ),
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          tx.title,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16,
-                          ),
-                        ),
-                        Text(
-                          DateFormat.yMMMd().format(tx.date),
-                          style: const TextStyle(
-                            color: Colors.grey,
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-              );
-            }).toList(),
-          )
+          UserTransactions(),
         ],
       ),
     );
