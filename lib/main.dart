73ca129621331c3d984962ff98dc33053ab79f8a
EliezerAmbion <eliezerambion@gmail.com>
2022-11-12 14:32:44 +0800

    add a modal

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,104 @@
 import 'package:flutter/material.dart';
 
-import 'widgets/user_transactions.dart';
+import './widgets/new_transaction.dart';
+import './widgets/transaction_list.dart';
+import './models/transaction.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.teal),
       title: 'Flutter App',
       home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  late String titleInput;
-  late String amountInput;
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> _userTransactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
+  ];
+
+  void _addNewTransaction(String txTitle, double txAmount) {
+    final newTx = Transaction(
+      title: txTitle,
+      amount: txAmount,
+      date: DateTime.now(),
+      id: DateTime.now().toString(),
+    );
+
+    setState(() {
+      _userTransactions.add(newTx);
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+      context: ctx,
+      builder: (_) {
+        return GestureDetector(
+          onTap: () {},
+          behavior: HitTestBehavior.opaque,
+          child: NewTransaction(addTx: _addNewTransaction),
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Flutter App'),
-      ),
-      body: Column(
-        // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Container(
-            width: double.infinity,
-            child: const Card(
-              color: Colors.blue,
-              elevation: 5,
-              child: Text('CHART'),
-            ),
+        title: const Text('Expense Tracker'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () => _startAddNewTransaction(context),
           ),
-          UserTransactions(),
         ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: const Card(
+                color: Colors.teal,
+                elevation: 5,
+                child: Text('CHART!'),
+              ),
+            ),
+            TransactionList(transactions: _userTransactions),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () => _startAddNewTransaction(context),
       ),
     );
   }
--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,15 +1,38 @@
 import 'package:flutter/material.dart';
 
-class NewTransaction extends StatelessWidget {
-  // study more about TextEditingController()
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
+class NewTransaction extends StatefulWidget {
   final Function addTx;
 
   NewTransaction({
     super.key,
     required this.addTx,
   });
+
+  @override
+  State<NewTransaction> createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
+  final titleController = TextEditingController();
+
+  final amountController = TextEditingController();
+
+  void submitData() {
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+
+    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+      return;
+    }
+
+    widget.addTx(
+      enteredTitle,
+      enteredAmount,
+    );
+
+    // to close that top most screen that is displayed, i.e. the modal in this example.
+    Navigator.of(context).pop();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,23 +42,52 @@ class NewTransaction extends StatelessWidget {
         padding: const EdgeInsets.all(10),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
+          children: <Widget>[
             TextField(
-              decoration: const InputDecoration(labelText: 'Title: '),
+              decoration: InputDecoration(
+                labelText: 'Title',
+                suffixIcon: IconButton(
+                  onPressed: () {
+                    titleController.clear();
+                  },
+                  icon: const Icon(
+                    Icons.clear,
+                    color: Colors.black,
+                  ),
+                  iconSize: 18,
+                ),
+              ),
               controller: titleController,
+              // you can add (_) => submitData() to add data on the press of the check in the keyboard
+              // notice that it has a parenthesis ()
+              // NOTE: you commented it out because you don't want that behaviour
+              // onSubmitted: (_) => submitData(),
             ),
             TextField(
-              decoration: const InputDecoration(labelText: 'Amount: '),
+              decoration: InputDecoration(
+                labelText: 'Amount',
+                suffixIcon: IconButton(
+                  onPressed: () {
+                    amountController.clear();
+                  },
+                  icon: const Icon(
+                    Icons.clear,
+                    color: Colors.black,
+                  ),
+                  iconSize: 18,
+                ),
+              ),
               controller: amountController,
+              keyboardType: TextInputType.number,
+              // you can add (_) => submitData() to add data on the press of the check in the keyboard
+              // notice that it has a parenthesis ()
+              // NOTE: you commented it out because you don't want that behaviour
+              // onSubmitted: (_) => submitData(),
             ),
             TextButton(
-              onPressed: () {
-                addTx(
-                  titleController.text,
-                  double.parse(amountController.text),
-                );
-              },
-              style: TextButton.styleFrom(foregroundColor: Colors.amber),
+              style: TextButton.styleFrom(foregroundColor: Colors.deepPurple),
+              // this submitData has no parenthesis, study why
+              onPressed: submitData,
               child: const Text('Add Transaction'),
             ),
           ],
